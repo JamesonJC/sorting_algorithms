cc5e17ce749c05c03bea31d58738f66cd1d3d050
JamesonJc <jamesonpillay@gmail.com>
2023-09-26 18:41:42 +0000

    2. Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+  * selection_sort - Sort the elements of an array with selection sort algo.
+  * @array: Arrat to be sorted.
+  * @size: The size of the array.
+  */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0, j, min_index;
+	int tmp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (i < size - 1)
+	{
+		min_index = i;
+
+		/*Getting minimum in the remaining elements*/
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
+		/*Swap the minimum element and the current*/
+		if (min_index != i)
+		{
+			tmp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = tmp;
+			print_array(array, size);
+		}
+		i++;
+	}
+}
