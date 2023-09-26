0f66adeb3eada6d9d717212321d1980eb714875e
Sabelodone <sabelozondo825@gmail.com>
2023-09-26 22:20:41 +0000

    update spaces

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,30 +8,28 @@
 
 void selection_sort(int *array, size_t size)
 {
-        size_t i = 0, j, min_index;
-        int tmp;
+size_t i = 0, j, min_index;
+int tmp;
+if (array == NULL || size < 2)
+return;
 
-        if (array == NULL || size < 2)
-                return;
-
-        while (i < size - 1)
-        {
-                min_index = i;
-
-                /*Getting minimum on the remaining elements*/
-                for (j = i + 1; j < size; j++)
-                {
-                        if (array[j] < array[min_index])
-                                min_index = j;
-                }
-                /*Swap the minimum element and the current*/
-                if (min_index != i)
-                {
-                        tmp = array[i];
-                        array[i] = array[min_index];
-                        array[min_index] = tmp;
-                        print_array(array, size);
-                }
-                i++;
-        }
+while (i < size - 1)
+{
+min_index = i;
+/*Getting minimum on the remaining elements*/
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[min_index])
+min_index = j;
+}
+/*Swap the minimum element and the current*/
+if (min_index != i)
+{
+tmp = array[i];
+array[i] = array[min_index];
+array[min_index] = tmp;
+print_array(array, size);
+}
+i++;
+}
 }
