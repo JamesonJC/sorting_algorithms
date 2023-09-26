9418236bc75682c764acbf9523f7791b89ef0490
Sabelodone <sabelozondo825@gmail.com>
2023-09-26 21:19:38 +0000

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,7 +15,7 @@ void bubble_sort(int *array, size_t size)
         if (array == NULL || size < 2)
                 return;
 
-        for (; i < size - 1; i++)
+        for (i = 0; i < size - 1; i++)
         {
                 for (j = 0; j < size - 1 - i; j++)
                 {
