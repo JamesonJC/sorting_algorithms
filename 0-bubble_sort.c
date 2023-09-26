17dd46c9093f498c0eabbf6336a2cd0d0b64393e
Sabelodone <sabelozondo825@gmail.com>
2023-09-26 22:02:34 +0000

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,25 +8,24 @@
 
 void bubble_sort(int *array, size_t size)
 {
-        size_t i = 0, j = 0;
-        int temp;
+size_t i = 0, j = 0;
+int temp;
 
-        /*Return if array is NULL or there's only one or less elements*/
-        if (array == NULL || size < 2)
-                return;
-
-        for (i = 0; i < size - 1; i++)
-        {
-                for (j = 0; j < size - 1 - i; j++)
-                {
-                        if (array[j + 1] < array[j])
-                        {
-                                /*swap elements if they are in the wrong order*/
-                                temp = array[j];
-                                array[j] = array[j + 1];
-                                array[j + 1] = temp;
-                                print_array(array, size);
-                        }
-                }
-        }
+/*Return if array is NULL or there's only one or less elements*/
+if (array == NULL || size < 2)
+return;
+for (i = 0; i < size - 1; i++)
+{
+for (j = 0; j < size - 1 - i; j++)
+{
+if (array[j + 1] < array[j])
+{
+/*swap elements if they are in the wrong order*/
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+print_array(array, size);
+}
+}
+}
 }
